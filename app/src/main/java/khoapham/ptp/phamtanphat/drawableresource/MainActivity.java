d72e96b713c6b7f9c19db981cc62c7ad75350076
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-09 19:04:31 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/drawableresource/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/drawableresource/MainActivity.java
@@ -1,13 +1,50 @@
 package khoapham.ptp.phamtanphat.drawableresource;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgClipDrawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgClipDrawable = findViewById(R.id.imageviewClipDrawable);
+
+
+
+        imgClipDrawable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CountDownTimer countDownTimer = new CountDownTimer(10000,1000) {
+                    @Override
+                    public void onTick(long time) {
+
+                        imgClipDrawable.setImageLevel(imgClipDrawable.getDrawable().getLevel() + 1000);
+                        Log.d("BBB",time + "" );
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        imgClipDrawable.setImageLevel(imgClipDrawable.getDrawable().getLevel() + 1000);
+                        if(imgClipDrawable.getDrawable().getLevel() == 10000){
+                            imgClipDrawable.setImageLevel(0);
+                            this.start();
+                        }
+
+                    }
+                };
+                countDownTimer.start();
+
+
+            }
+        });
     }
 }
