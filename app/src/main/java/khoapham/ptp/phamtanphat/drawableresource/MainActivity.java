b7a288bc27438f7acfed3da99b42e0ed6ffee995
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-11 18:28:09 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/drawableresource/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/drawableresource/MainActivity.java
@@ -1,47 +1,75 @@
 package khoapham.ptp.phamtanphat.drawableresource;
 
+import android.graphics.drawable.Drawable;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgClipDrawable;
+    ImageView imgClipDrawable , imgLevellist;
+    Button btnCong,btnTru;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgClipDrawable = findViewById(R.id.imageviewClipDrawable);
+//        imgClipDrawable = findViewById(R.id.imageviewClipDrawable);
+//
+//
+//
+//        imgClipDrawable.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                CountDownTimer countDownTimer = new CountDownTimer(10000,1000) {
+//                    @Override
+//                    public void onTick(long time) {
+//
+//                        imgClipDrawable.setImageLevel(imgClipDrawable.getDrawable().getLevel() + 1000);
+//                        Log.d("BBB",time + "" );
+//                    }
+//
+//                    @Override
+//                    public void onFinish() {
+//                        imgClipDrawable.setImageLevel(imgClipDrawable.getDrawable().getLevel() + 1000);
+//                        if(imgClipDrawable.getDrawable().getLevel() == 10000){
+//                            imgClipDrawable.setImageLevel(0);
+//                            this.start();
+//                        }
+//
+//                    }
+//                };
+//                countDownTimer.start();
+//            }
+//        });
+        imgLevellist = findViewById(R.id.imageviewEnergy);
+        btnCong = findViewById(R.id.buttonCong);
+        btnTru = findViewById(R.id.buttonTru);
 
 
-
-        imgClipDrawable.setOnClickListener(new View.OnClickListener() {
+        btnCong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CountDownTimer countDownTimer = new CountDownTimer(10000,1000) {
-                    @Override
-                    public void onTick(long time) {
-
-                        imgClipDrawable.setImageLevel(imgClipDrawable.getDrawable().getLevel() + 1000);
-                        Log.d("BBB",time + "" );
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        imgClipDrawable.setImageLevel(imgClipDrawable.getDrawable().getLevel() + 1000);
-                        if(imgClipDrawable.getDrawable().getLevel() == 10000){
-                            imgClipDrawable.setImageLevel(0);
-                            this.start();
-                        }
-
-                    }
-                };
-                countDownTimer.start();
+                if (imgLevellist.getDrawable().getLevel() == 100){
+                    imgLevellist.setImageLevel(0);
+                }else{
+                    imgLevellist.setImageLevel(imgLevellist.getDrawable().getLevel() + 10);
+                }
+            }
+        });
+        btnTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (imgLevellist.getDrawable().getLevel() == 0){
+                    imgLevellist.setImageLevel(100);
+                }else{
+                    imgLevellist.setImageLevel(imgLevellist.getDrawable().getLevel() - 10);
+                }
             }
         });
     }
